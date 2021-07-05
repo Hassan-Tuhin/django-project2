9df009c8f7de9707b198c53de9e08e3ac4494f94
Hassan-Tuhin <hassantohin27@gmail.com>
2021-07-05 10:20:59 +0600

    2nd change

--- index.js
+++ index.js
@@ -7,4 +7,48 @@ console.error("This is error!!");
 
 var number1=10;
 var number2=20;
-console.log(number1 * number2); //This output will be shown in concole panel
+//console.log(number1 * number2); //This output will be shown in concole panel
+
+//numbers:
+var num1=27;
+var num2=41;
+
+//strings:
+var str1="This is first string";
+var str2="This is second string";
+
+//objects:
+var marks={
+    Hassan:1,
+    Tohin:2,
+    Saifu:3
+}
+
+//console.log(marks);
+//boolean:
+var1=true;
+var2=false;
+//console.log(var1, var2);
+
+var und;
+//console.log(und);//means no value defined here
+
+var n=null;
+//console.log(n);
+
+var arr=[1,2,3,'Hassan',5];
+//console.log(arr);
+//console.log(arr[1]);
+
+//arithmetic operators:
+var a=5;
+var b=6;
+console.log("The value of a+b is ",a+b)
+console.log("The value of a-b is ",a-b)
+console.log("The value of a*b is ",a*b)
+console.log("The value of a/b is ",a/b)
+
+//assignment operators:
+var c=b;
+c+=2;
+console.log("The value of a+c is ",a+c)
