e6c580890ee5a8e6d8be7f9dda7054a34cfc81ee
Hassan-Tuhin <hassantohin27@gmail.com>
2021-07-05 08:45:56 +0600

    first change

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+console.log("Hello");
+//document.write("This is a document");
+console.warn("This is warning");
+console.error("This is error!!");
+*/
+
+var number1=10;
+var number2=20;
+console.log(number1 * number2); //This output will be shown in concole panel
