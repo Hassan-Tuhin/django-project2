a625522301a289e53ab731793f8ce745beabf7b1
Hassan-Tuhin <hassantohin27@gmail.com>
2021-07-06 09:42:52 +0600

    7-6-2021

--- index.js
+++ index.js
@@ -5,42 +5,43 @@ console.warn("This is warning");
 console.error("This is error!!");
 */
 
-var number1=10;
-var number2=20;
+var number1 = 10;
+var number2 = 20;
 //console.log(number1 * number2); //This output will be shown in concole panel
 
 //numbers:
-var num1=27;
-var num2=41;
+var num1 = 27;
+var num2 = 41;
 
 //strings:
-var str1="This is first string";
-var str2="This is second string";
+var str1 = "This is first string";
+var str2 = "This is second string";
 
 //objects:
-var marks={
-    Hassan:1,
-    Tohin:2,
-    Saifu:3
+var marks = {
+    Hassan: 1,
+    Tohin: 2,
+    Saifu: 3
 }
 
 //console.log(marks);
 //boolean:
-var1=true;
-var2=false;
+var1 = true;
+var2 = false;
 //console.log(var1, var2);
 
 var und;
 //console.log(und);//means no value defined here
 
-var n=null;
+var n = null;
 //console.log(n);
 
-var arr=[1,2,3,'Hassan',5];
+var arr = [1, 2, 3, 'Hassan', 5];
 //console.log(arr);
 //console.log(arr[1]);
 
 //arithmetic operators:
+/*
 var a=5;
 var b=6;
 console.log("The value of a+b is ",a+b)
@@ -52,3 +53,12 @@ console.log("The value of a/b is ",a/b)
 var c=b;
 c+=2;
 console.log("The value of a+c is ",a+c)
+*/
+//Using loops
+var arr = [1, 2, 3, 4, 5, "Hassan", "Tuhin", "BYT"];
+
+let j = 0;
+do {
+    console.log(arr[j]);
+    j++;
+} while (j < arr.length);
