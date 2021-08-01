6ca03e4d57d7e327f7ee3d2899102819bf138b18
Hassan-Tuhin <hassantohin27@gmail.com>
2021-08-01 11:11:25 +0600

    1-8-2021

---
+++ import.js
@@ -0,0 +1,49 @@
+import {pi} from './export.js';
+console.log(pi);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function modifier(strings,...values){
+//     console.log(strings);
+//     console.log(values)
+// }
+
+
+// var player1="sakib";
+// var player2="tamim";
+// console.log(modifier `we have ${player1} and ${player2} best player in our team`);
+
+
+
+
+// function modifier(strings,...values){
+//     const m=strings.reduce((prev,current)=>{
+//         return prev + current + (values.length ? "Mr. " +values.shift() : "");
+//     },"");
+//     return m;
+// }
+
+
+// var player1="sakib";
+// var player2="tamim";
+// console.log(modifier `we have ${player1} and ${player2} best player in our team`);
+
+
+
